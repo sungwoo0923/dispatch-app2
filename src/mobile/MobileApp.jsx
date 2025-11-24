dcfff0829da35c4efb1871a80c91f05ecfb7d8dc
성우0923 <tjddnqkf@naver.com>
2025-11-24 16:58:27 +0900

    모바일16

--- src/mobile/MobileApp.jsx
+++ src/mobile/MobileApp.jsx
@@ -8,7 +8,7 @@ import {
   onSnapshot,
   serverTimestamp,
 } from "firebase/firestore";
-import { db } from "../firebase";
+import { db } from "../../firebase";
 
 // ------------------------------------------------------------------
 // 공통 유틸
