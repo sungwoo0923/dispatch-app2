b5565d1d212a64adb30b3b4515f9622ab4b42456
성우0923 <tjddnqkf@naver.com>
2025-12-30 14:11:56 +0900

    재수정

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.f4jt1epbg9g"
+    "revision": "0.5l9v25l6obo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
