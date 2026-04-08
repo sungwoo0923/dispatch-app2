2d9b3463431a70b21ad4853078bf5d7239ddf316
성우0923 <tjddnqkf@naver.com>
2026-04-08 12:12:49 +0900

    업데이트

--- api/route.js
+++ api/route.js
@@ -8,8 +8,9 @@ function cleanAddress(addr = "") {
     .trim();
 }
 
+
 const handler = async (req, res) => {
-  console.log("🔥🔥🔥 route API 실행됨");
+  
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method Not Allowed" });
   }
